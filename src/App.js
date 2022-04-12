b401f40b164dbf21a0c2b01422c215a26c2ddab4
Prakash Reddy Potlapadu <reddy.prakash874@gmail.com>
2022-04-12 09:31:19 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React,{useReducer} from "react";
+import React, { useReducer } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import routes  from "./routes";
-import './App.scss'
-import {UserContext} from './containers/SingedUpUsers/context/context'
-import {initialUserState,userReducer} from './containers/SingedUpUsers/context/userReducer'
+import routes from "./routes";
+import "./App.scss";
+import { UserContext } from "./containers/SingedUpUsers/context/context";
+import { initialUserState, userReducer } from "./containers/SingedUpUsers/context/userReducer";
 
 function App() {
-  const [userState,dispatch]=useReducer(userReducer,initialUserState);
-  return (
-    <UserContext.Provider value={{data:userState,dispatch:dispatch}}>
-    <BrowserRouter>
-        <Switch>
-            {routes.map((route, index) => (
-                <Route key={index} exact path={route.path} component={route.component} />
-            ))}
-        </Switch>
-    </BrowserRouter>
-    </UserContext.Provider>
-  );
+    const [userState, dispatch] = useReducer(userReducer, initialUserState);
+    return (
+        <BrowserRouter>
+            <UserContext.Provider value={{ data: userState, dispatch: dispatch }}>
+                <Switch>
+                    {routes.map((route, index) => (
+                        <Route key={index} exact path={route.path} component={route.component} />
+                    ))}
+                </Switch>
+            </UserContext.Provider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
